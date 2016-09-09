3068b90d48221a9df027ae14d486e4db89dd9ca7
diegopaniago <diegopaniago1@gmail.com>
2016-09-09 15:47:00 -0400

    Ajustando configuracao de rotas

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
 var enviar = require('./routes/enviarEmail');
 
 var app = express();
@@ -23,8 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+// routes
 app.use('/', enviar);
 
 // catch 404 and forward to error handler
