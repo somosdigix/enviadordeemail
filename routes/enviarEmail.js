a0206f030ad047ad6664537ca094363e081d1ac2
diegopaniago <diegopaniago1@gmail.com>
2016-09-19 17:32:31 -0400

    Comentando a rota de visualizacao

--- routes/enviarEmail.js
+++ routes/enviarEmail.js
@@ -11,8 +11,8 @@ router.post('/enviar', function(req, res, next) {
 	});
 });
 
-router.get('/', function(req, res, next) {
-	res.render('index', {});
-});
+//router.get('/', function(req, res, next) {
+//	res.render('index', {});
+//});
 
 module.exports = router;
