e9c5050767309afff24bbf2a7945546187b7c989
diegopaniago <diegopaniago1@gmail.com>
2016-09-22 10:48:08 -0400

    Refatorando view de teste

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,48 +1,6 @@
-(function ($) {
-	$.fn.serializeFormJSON = function () {
-
-		var o = {};
-		var a = this.serializeArray();
-		$.each(a, function () {
-			if (o[this.name]) {
-				if (!o[this.name].push) {
-					o[this.name] = [o[this.name]];
-				}
-				o[this.name].push(this.value || '');
-			} else {
-				o[this.name] = this.value || '';
-			}
-		});
-		return o;
-	};
-})(jQuery);
-
 $(document).ready(function(){
-	$('#formulario').on('submit', submitDoForm);
 	$('#formularioJson').on('submit', submitDoFormJson);
 });
-
-function submitDoForm(evento){
-	evento.preventDefault();
-	var json = $('#formulario').serializeFormJSON();
-	
-	var data = {};
-	data.options = {};
-	data.options.smtp = json.smtp;
-	data.options.mailOptions = {};
-	data.options.mailOptions.from = json.from;
-	data.options.mailOptions.to = json.to;
-	data.options.mailOptions.subject = json.subject;
-	data.options.mailOptions.html = json.html;
-	
-	$.post('/enviar', data)
-	.fail(function(){
-		$('#mensagemDeErro').text('Erro no POST');
-	})
-	.always(function( data, textStatus, error){
-		$('#mensagemDeErro').text(data);
-	});
-}
 
 function submitDoFormJson(evento){
 	evento.preventDefault();
